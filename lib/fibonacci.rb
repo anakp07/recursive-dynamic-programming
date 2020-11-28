49dcc080b064fcc87a62df41a61350debc3be24d
CheezItMan <chris@adadevelopersacademy.org>
2020-11-28 13:06:49 -0800

    add hint and notimplementederror

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -3,6 +3,7 @@
 # Time Complexity - ?
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
+# Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
- 
+  raise NotImplementedError, "Please implement this method for the project"
 end
