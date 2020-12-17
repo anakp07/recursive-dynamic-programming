56df70a087e9186e4e7eec54e09c44c9b27c61aa
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-12-16 16:11:34 -0800

    working on fibonacci runction

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -7,3 +7,37 @@
 def fibonacci(n)
   raise NotImplementedError, "Please implement this method for the project"
 end
+
+# Iterative Solution
+# def fibonacci(n)
+#   if n == 0 || n == 1
+#     return n
+#   end
+
+#   solutions = [0, 1]
+#   current = 2
+
+#   while current < n
+#     solutions << solutions[current - 1] + solutions[current - 2]
+#     current += 1
+#   end
+
+#   return solutions[current - 1] + solutions[current - 2]
+# end
+
+# Recursive Solution
+
+def fibonacci_recursive(n)
+  return fib_helper([0, 1], 2, n)
+end
+
+def fib_helper(solutions, current, n)
+  return n if n == 0 || n == 1
+
+  if current == n
+    return solutions[n - 1] + solutions[n-2]
+  end
+
+  solutions << solutions[current - 1] + solutions[current -2]
+  return fib_helper(solutions, current + 1, n)
+end
